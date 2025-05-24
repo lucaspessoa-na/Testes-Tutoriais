f9646b157d6650b14bcc7cec792ce1e74f7e7ed2
Lucas Pessôa <lucaspessoa.na@gmail.com>
2025-05-24 20:32:27 -0300

    Second stage

--- Dog.py
+++ Dog.py
@@ -1,2 +1,13 @@
+#creating the dog class
 class Dog:
-    pass
+    #atribute of every object in this class
+    species = "Canis Familiaries"
+    def __init__(self, name, age):
+        #each object in the class should have these specified
+        self.name = name
+        self.age = age
+#creating an object of the class
+carlos = Dog("Carlos", 3)
+palito = Dog("Palito", 4)
+#acess the instance info with (object.info)
+print(carlos.name)
